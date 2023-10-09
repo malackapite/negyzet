cc55057316ae44a1193c635534002ed632847782
Veress Marcell <veres.marcell@dk.akkszalezi.hu>
2023-10-09 13:22:20 +0200

    négyzet

--- src/negyzet/Negyzet.java
+++ src/negyzet/Negyzet.java
@@ -2,10 +2,19 @@
 package negyzet;
 
 public class Negyzet {
-    private double a, terulet, x, y;
+    private double a, terulet;
+    private Koordinata koordinata;
 
+    public void setKoordinata(Koordinata koordinata) {
+        this.koordinata = koordinata;
+    }
+
+    public Koordinata getKoordinata() {
+        return koordinata;
+    }
+    
     public void setA(double a) {
-        this.a = a;
+        this.a = a<=0?1:a;
     }
 
     public double getA() {
@@ -15,6 +24,4 @@ public class Negyzet {
     public double getTerulet() {
         return a*a;
     }
-    
-    
 }
--- src/negyzet/View.java
+++ src/negyzet/View.java
@@ -3,7 +3,7 @@ package negyzet;
 public class View {
     void kiIr(){
          Negyzet negyzet = new Negyzet();
-         negyzet.setA(2.2);
+         negyzet.setA(.1);
          System.out.println("A: "+negyzet.getA());
          System.out.println("Terület: "+ negyzet.getTerulet());
     }
