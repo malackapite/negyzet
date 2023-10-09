577630929309e7bd987051d2c21d0de0b364428b
Veress Marcell <veres.marcell@dk.akkszalezi.hu>
2023-10-09 13:08:08 +0200

    négyzet

--- src/negyzet/Negyzet.java
+++ src/negyzet/Negyzet.java
@@ -2,26 +2,18 @@
 package negyzet;
 
 public class Negyzet {
-    private double a,b, terulet, x, y;
+    private double a, terulet, x, y;
 
     public void setA(double a) {
         this.a = a;
-    }
-
-    public void setB(double b) {
-        this.b = b;
     }
 
     public double getA() {
         return a;
     }
 
-    public double getB() {
-        return b;
-    }
-
     public double getTerulet() {
-        return a*b;
+        return a*a;
     }
     
     
--- src/negyzet/View.java
+++ src/negyzet/View.java
@@ -4,9 +4,7 @@ public class View {
     void kiIr(){
          Negyzet negyzet = new Negyzet();
          negyzet.setA(2.2);
-         negyzet.setB(2);
          System.out.println("A: "+negyzet.getA());
-         System.out.println("B: "+negyzet.getB());
          System.out.println("Terület: "+ negyzet.getTerulet());
     }
 }
